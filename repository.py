0d9a77e1d5ed2c1e0d2f8c0dd2aa07657b1f3ae2
Michael Sexton <mick.sexton@gmail.com>
2014-08-27 17:47:53 +1000

    Reads xlsx then checks against directory structure

--- repository.py
+++ repository.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
+ACTIVITY_NAME_COL = 3
+STAGED_COL = 9
+COPIED_COL = 8
+NOPTA_FILE = '/Users/michael/Public/nopims/NOPTA_20120101_20140728_OpenFile_Well_List.xlsx'
+WELLS_ROOT = '/nas/pmd/repos/open/Wells/Regulated'
+
+DIRS = ['/nas/pm','']
+
 import os
+import openpyxl as px
 
-for dirname, dirnames, filenames in os.walk('/nas'):
-  for subdirname in dirnames:
-    print os.path.join(dirname, subdirname)
+workbook = px.load_workbook(NOPTA_FILE,use_iterators = True)
 
-  for filename in filenames:
-    print os.path.join(dirname, filename)
+sheet = workbook.get_sheet_by_name(name = "NOPTA-OF-Wells")
+
+for row in sheet.iter_rows():
+    staged = row[STAGED_COL].value
+    copied = row[COPIED_COL].value
+    if not staged and not copied:
+        activity_name = row[ACTIVITY_NAME_COL].value
+        directory_string = activity_name.replace(' ','_')
+        for root, dirs, files in os.walk(WELLS_ROOT):
+            print dirs
+    
+
+#
+#  for filename in filenames:
+#    print os.path.join(dirname, filename)
