c74d92f5b533c20a0930749a544a268d35851f37
Michael Sexton <mick.sexton@gmail.com>
2014-08-28 13:20:21 +1000

    Changed to xlrd from openpyxl

--- repository.py
+++ repository.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python
 ACTIVITY_NAME_COL = 3
+TITLE_COL = 4
 STAGED_COL = 9
 COPIED_COL = 8
-NOPTA_FILE = '/Users/michael/Public/nopims/NOPTA_20120101_20140728_OpenFile_Well_List.xlsx'
+NOPTA_FILE = "/nas/energy/ideas/RDIS/NOPIMS repository documents remediation/NOPTA_20120101_20140728_OpenFile_Well_List.xlsx"
 NOPTA_SHEET_NAME = "NOPTA-OF-Wells"
-WELLS_ROOT = '/nas/pmd/repos/open/Wells/Regulated'
+WELLS_ROOT = "/nas/pmd/repos/open/Wells/Regulated"
 
 
 import os
-import openpyxl as px
+import xlrd
 
-workbook = px.load_workbook(NOPTA_FILE,use_iterators = True)
+workbook = xlrd.open_workbook(NOPTA_FILE)
 
-sheet = workbook.get_sheet_by_name(name = NOPTA_SHEET_NAME)
+sheet = workbook.sheet_by_name(NOPTA_SHEET_NAME)
 
-for row in sheet.iter_rows():
+num_rows = sheet.nrows
+i = 1
+
+while i < num_rows:
+    
+    row = sheet.row(i)
     staged = row[STAGED_COL].value
     copied = row[COPIED_COL].value
     if not staged and not copied:
+        state_folder = row[TITLE_COL].value.split('/')[0].split('-')[0]
         activity_name = row[ACTIVITY_NAME_COL].value
-        directory_string = activity_name.replace(' ','_')
-        for root, dirs, files in os.walk(WELLS_ROOT):
-            print dirs
+        print state_folder
+        #print activity_name
+    i += 1
+        #directory_string = activity_name.replace(' ','_')
+        #for root, dirs, files in os.walk(WELLS_ROOT):
+            #print dirs
     
 
 #
