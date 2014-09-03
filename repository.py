c8efbee199f4e393a8d47719cf774465e08b5e04
Michael Sexton <mick.sexton@gmail.com>
2014-09-03 16:16:46 +1000

    Add parantheses

--- repository.py
+++ repository.py
@@ -43,7 +43,7 @@ def run_me():
     print associated_wells.__len__()
     #copy_wells
     
-def copy_wells:
+def copy_wells():
     global associated_wells
     for aw in associated_wells:
         
