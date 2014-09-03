9d36291b8f7a2214c86ce61cc41b85ff040bcbe1
Michael Sexton <mick.sexton@gmail.com>
2014-09-03 22:11:59 +1000

    Now do it

--- repository.py
+++ repository.py
@@ -47,10 +47,8 @@ def copy_wells():
     for aw in associated_wells:
         for a in aw["activities"]:
             for p in a["paths"]:
-                copy_string = "cp -r {0} {1}".format(p,DEST_DIR)
-                print copy_string
-                #os.system("cp -r %s %s".format(p,DEST_DIR))
-                #os.system("chmod -R 775 %s".format(DEST_DIR))
+                os.system("cp -r {0} {1}".format(p,DEST_DIR))
+    os.system("chmod -R 775 {0}".format(DEST_DIR))
         
 
 def associate_wells_and_paths(t,an,p):
