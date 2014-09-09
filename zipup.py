5523127665ac31049b3258c0a8e0fb01bf93f673
Michael Sexton <mick.sexton@gmail.com>
2014-09-09 15:33:44 +1000

    Check the root

--- zipup.py
+++ zipup.py
@@ -3,8 +3,9 @@ import os
 
 def zipdir(path, z):
     for root, dirs, files in os.walk(path,topdown=False):
-        for file in files:
-            z.write(os.path.join(root, file))
+        print root
+        #for file in files:
+            #z.write(os.path.join(root, file))
             
 def main(root_path='/nas/energy/ideas/RDIS/NOPIMS_repository_remediation/msutti/01.To_Zip'):
     for f in os.listdir(root_path):
