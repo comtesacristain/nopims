b275236684f7a6790a6d38980449e5b9da37e871
Michael Sexton <mick.sexton@gmail.com>
2014-09-10 11:12:53 +1000

    Test rmtree

--- zipup.py
+++ zipup.py
@@ -1,5 +1,5 @@
 from zipfile import ZipFile
-import os
+import os, shutil
 
 def zipdir(path, z):
     for root, dirs, files in os.walk(path):
@@ -7,7 +7,7 @@ def zipdir(path, z):
             z.write(os.path.join(root, file), os.path.join(os.path.relpath(root,path),file))
 
             
-def main(root_path='/nas/energy/ideas/RDIS/NOPIMS_repository_remediation/msutti/01.To_Zip'):
+def main(root_path='/nas/energy/ideas/RDIS/NOPIMS_repository_remediation/msutti/pytest'):
     for f in os.listdir(root_path):
         folder = os.path.join(root_path,f)
         to_zips = os.listdir(folder)
@@ -16,4 +16,5 @@ def main(root_path='/nas/energy/ideas/RDIS/NOPIMS_repository_remediation/msutti/
             with ZipFile(zf+".zip", 'w',allowZip64=True) as z:
                 print "Zipping path " + zf
                 zipdir(zf,z)
+            shutil.rmtree(zf)
 main()
